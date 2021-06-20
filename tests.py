018cc4a688df9a7232ddaafafed253d97854615a
EiTamOnya <60297174+EiTamOnya@users.noreply.github.com>
2021-06-21 00:35:16 +0300

    Added tests for recent and top song pages (#3)

--- tests.py
+++ tests.py
@@ -2,12 +2,7 @@
 import requests
 import unittest
 
-from page import (
-    LoginPage,
-    SpotifyLoginPage,
-    SpotifyPlayerPage,
-    MainPage,
-    HOME_URL)
+from page import LoginPage, SpotifyLoginPage, SpotifyPlayerPage, MainPage, HOME_URL
 
 from selenium import webdriver
 
@@ -19,12 +14,14 @@ class LyricspotTestCase(unittest.TestCase):
     def setUpClass(cls):
         # Make sure that the dyno is awake before continuing with the tests
         resp = requests.get(HOME_URL)
-        assert(resp.status_code == 200)
+        assert resp.status_code == 200
 
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--mute-audio")
-        self.driver = webdriver.Chrome(r'driver\\chromedriver.exe', chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(
+            r"driver\\chromedriver.exe", chrome_options=chrome_options
+        )
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
         self.login_page = LoginPage(self.driver)
@@ -36,35 +33,57 @@ class LyricspotTestCase(unittest.TestCase):
     def test_page_title(self):
         """Test the title of the webpage."""
         self.driver.get(HOME_URL)
-        self.assertIn('Lyricspot', self.driver.title)
+        self.assertIn("Lyricspot", self.driver.title)
 
     def test_logging_in_and_out(self):
         """Test the logging in functionality."""
         self.login_page.log_in()
-        self.assertEquals('Log out', self.main_page.get_logout_link().text)
+        self.assertEquals("Log out", self.main_page.get_logout_link().text)
         self.main_page.get_logout_link().click()
-        self.assertEquals('Login with Spotify', self.login_page.get_login_button().text)
+        self.assertEquals("Login with Spotify", self.login_page.get_login_button().text)
 
     def test_lyrics_button(self):
         self.login_page.log_in()
-        self.assertEquals('Show Lyrics', self.main_page.get_lyrics_button().text)
+        self.assertEquals("Show Lyrics", self.main_page.get_lyrics_button().text)
         self.main_page.click_show_lyrics()
-        self.assertEquals('Hide Lyrics', self.main_page.get_lyrics_button().text)
+        self.assertEquals("Hide Lyrics", self.main_page.get_lyrics_button().text)
 
     def test_check_lyrics(self):
         """Test for checking if the correct song lyrics are shown."""
-        self.spotify_player.login_and_play_song('https://open.spotify.com/album/2XPrwlaAHHXnJzP9tBcIzH')
+        self.spotify_player.login_and_play_song(
+            "https://open.spotify.com/album/2kKXGWaCEl06EKZ4DxBJIT"
+        )
 
         self.login_page.click_login_button()
         self.main_page.click_show_lyrics()
         self.main_page.wait_for_lyrics()
 
-        expected_name = 'Swimming Pools (Drank)'
-        expected_text = 'Pool full of liquor, then you dive in it'
+        expected_name = "Dream House"
+        expected_text = "I want to dream"
 
         self.assertEqual(expected_name, self.main_page.get_song_name().text)
         self.assertTrue(expected_text in self.main_page.get_lyrics_content().text)
-    
 
-if __name__ == '__main__':
+    def test_check_top_tracks(self):
+        """Test for checking if the top tracks are shown."""
+        self.login_page.log_in()
+        self.main_page.get_top_tracks().click()
+        self.assertTrue(
+            "Your 50 Top Played Tracks" in self.main_page.get_page_title().text
+        )
+        artists = [artist.text for artist in self.main_page.get_all_song_artists()]
+        self.assertTrue("Deafheaven" in artists)
+
+    def test_check_recent_tracks(self):
+        """Test for checking if the top tracks are shown."""
+        self.login_page.log_in()
+        self.main_page.get_recent_tracks().click()
+        self.assertTrue(
+            "Your 50 Recently Played Tracks" in self.main_page.get_page_title().text
+        )
+        song_names = [song.text for song in self.main_page.get_all_song_names()]
+        self.assertTrue("Dream House" in song_names)
+
+
+if __name__ == "__main__":
     unittest.main(verbosity=2)
