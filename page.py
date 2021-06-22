668e44f3ed04170734f349bc6c6d907d4ea6880a
EiTamOnya <pashov.pavel@gmail.com>
2021-06-23 01:54:22 +0300

    Changed the HOME url

--- page.py
+++ page.py
@@ -13,8 +13,7 @@ from locators import (
 )
 
 
-# HOME_URL = "https://lyric-spot.herokuapp.com/"
-HOME_URL = "http://127.0.0.1:5000"
+HOME_URL = "https://lyric-spot-staging.herokuapp.com/"
 username = os.getenv("USER")
 password = os.getenv("PASS")
 
