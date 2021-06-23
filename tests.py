e6d2db112791fd9b9728fb28a93af6a17ba191ad
EiTamOnya <pashov.pavel@gmail.com>
2021-06-23 23:28:49 +0300

    Added new workflow for running the tests manually
    Removed the headless argument to see if the tests will run in the container

--- tests.py
+++ tests.py
@@ -22,7 +22,7 @@ class LyricspotTestCase(unittest.TestCase):
         options = webdriver.ChromeOptions()
         options.add_argument("--mute-audio")
         if platform == "linux" or platform == "linux2":
-            options.add_argument("--headless")
+            # options.add_argument("--headless")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-gpu")
